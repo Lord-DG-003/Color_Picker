14cdf888479e17d2748c397f5daa240cdf4b22d9
Mridul Pal <mridulpal04@gamil.com>
2026-07-11 18:29:53 +0530

    Added polished library for dynamic text color and smooth transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import ColorPicker from './components/ColorPicker'
+import { readableColor } from 'polished'
+
 
 const App = () => {
 
   const [isVisible, setIsVisible]=useState(false);
   const [color,setColor]=useState("#ffffff");
+  const dynamicTextColor= readableColor(color);
 
   return (
-    <div id="page" className='h-screen w-full flex flex-col items-center relative' style={{backgroundColor: color}}>
+    <div id="page" className='h-screen w-full flex flex-col items-center relative transition-colors duration-150 ease-in-out' style={{backgroundColor: color, color:dynamicTextColor}}>
       <div className='items-center flex flex-col'>
          <Navbar isVisible={isVisible} setIsVisible={setIsVisible}/>
          {isVisible&&<ColorPicker color={color} setColor={setColor}/>}
       </div>
-      <div className='flex justify-center flex-1 absolute w-full  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-        <p className='font-bold text-[19px] text-center'>Set your background color by clicking to the color picker</p>
+      <div className='flex justify-center flex-1 absolute w-full  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-colors duration-300 ease-in-out'>
+        <p className='font-bold text-[19px] text-center'>Set your background color by clicking to the Show Color Picker written above</p>
       </div>
       
+
     </div>
   )
 }
